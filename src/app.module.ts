aa0de064f331c31d5455d2b2523ac01966133135
IsaacCruzLM <isaac.clm1@gmail.com>
2025-04-09 17:25:46 -0300

    feat: init the cardio-crm-api

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User],
+      synchronize: true, // cuidado: use apenas em dev
+    }),
+    UserModule,
+    AuthModule,
+  ],
+})
+export class AppModule {}
