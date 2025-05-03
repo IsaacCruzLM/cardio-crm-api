9e5b456bf8f1ba2f6467fd71cb8a5dc33085fd85
IsaacCruzLM <isaac.cruz@kartado.com.br>
2025-05-03 19:32:16 -0300

    chore: add new modules to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,19 +4,28 @@ import { UserModule } from 'src/user/user.module';
 import { User } from 'src/user/entities/user.entity';
 import { AuthModule } from 'src/auth/auth.module';
 import { PatientModule } from 'src/patient/patient.module';
+import { EvolucoesModule } from 'src/evolution/evolution.module';
+import { ConsultasModule } from 'src/appointment/appointment.module';
+import { SinaisVitaisModule } from 'src/vital_signs/vital_signs.module';
 import { Patient } from 'src/patient/entities/patient.entity';
+import { Consulta } from 'src/appointment/entities/appointment.entity';
+import { Evolucao } from 'src/evolution/entities/evolution.entity';
+import { SinalVital } from 'src/vital_signs/entities/vital_signs.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [User, Patient],
+      entities: [User, Patient, Consulta, Evolucao, SinalVital],
       synchronize: true, // cuidado: use apenas em dev
     }),
     UserModule,
     AuthModule,
     PatientModule,
+    ConsultasModule,
+    EvolucoesModule,
+    SinaisVitaisModule,
   ],
 })
 export class AppModule {}
